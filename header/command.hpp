fa00997ccecab055dd22ae877e62be04ca6ffc47
mehdi.hadidi <mehdi.hadidi@sahand.co>
2023-03-18 19:27:44 -0400

    added change directory and quit to ftp commands

--- header/command.hpp
+++ header/command.hpp
@@ -2,7 +2,10 @@
 
 namespace mh
 {
-    #define USER "USER"
-    #define PASSWORD "PASS"
-    #define LIST "LIST"
+    #define USER                         "USER"
+    #define PASSWORD                     "PASS"
+    #define LIST                         "LIST"
+    #define CHANGE_DIRECTORY             "CWD"
+    #define CHANGE_TO_PARENT_DIRECTORY   "CDUP"
+    #define LOGOUT                       "QUIT"
 }
